60b8b14d79af2e22a44883bddee261f94023380b
Blend Mehija <blendimehija@gmail.com>
2025-04-23 00:24:23 +0200

    updated css styling

---
+++ main/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+import os
+from django.utils.text import slugify
+
+# Create your models here.
+
+
+class Flavour(models.Model):
+      flavour_name = models.CharField(max_length=100, unique=True)
+      slug = models.SlugField(max_length=100, unique=True, db_index=True)
+      
+      def __str__(self):
+            return self.flavour_name
+      
+      def save(self, *args, **kwargs):
+        self.slug = slugify(self.flavour_name)
+        super().save(*args, **kwargs)
+
+      def delete(self, *args, **kwargs):
+            super().delete(*args, **kwargs)
+
+
+class CakeModel(models.Model):
+      name = models.CharField(max_length=100)
+      short_description = models.CharField(max_length=200,  null=True, blank=True)
+      description = models.TextField()
+      price = models.DecimalField(max_digits=10, decimal_places=2)
+      image = models.ImageField(upload_to="cakes_image")
+      flavour = models.ManyToManyField(Flavour, related_name="flavour", blank=True)
+      is_popular = models.BooleanField(default=False)
+      is_available = models.BooleanField(default=True)
+      created_at = models.DateTimeField(auto_now_add=True)
+      slug = models.SlugField(max_length=100, unique=True, db_index=True)
+
+
+      def __str__(self):
+            return self.name
+
+      class Meta:
+        verbose_name = "Cake"  # Singular name in admin
+        verbose_name_plural = "Cakes"
+
+      def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+      def delete(self, *args, **kwargs):
+        # Delete the associated image file first
+        if self.image:
+            if os.path.isfile(self.image.path):
+                os.remove(self.image.path)
+        # Then delete the database record
+        super().delete(*args, **kwargs)
---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
+    path('search/', views.search, name='search'),
+    path('cake/<slug:cake_slug>', views.SingleCakeView.as_view(), name="cake_info" ),
+    path('flavour/<slug:flavour_slug>', views.flavours, name='cake_by_flavour'),
+]
